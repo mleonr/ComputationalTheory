dc5f70beb3c0b311a51d2fb79096f1bc210d4f04
Marcos León Reyes <37048529+mleonr@users.noreply.github.com>
2018-09-16 22:33:04 -0500

    Add files via upload

---
+++ Entrega/nfa.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void automata(string cad);
+void rec1(vector<vector<string>>&arr);
+void rec2(vector<vector<string>>&arr);
+void lim_vec(vector<vector<string>>&arr,vector<string>&vec, string cad);
+
+void opcion1();
+void opcion2();
+
+int main(){
+
+	int n;
+	string cad;
+
+	srand(time(NULL));
+
+	while(true){
+		cout<<"1)Generar cadena"<<endl;
+		cout<<"2)Generar cadena aleatoriamente"<<endl;
+		cout<<"3)Salir"<<endl;
+		cin>>n;
+		//system("cls");
+		switch(n){
+			case 1:
+				opcion1();
+			break;
+			case 2:
+				opcion2();
+			break;
+			case 3:
+				return 0;
+			break;
+			default: 
+				cout<<"Opcion no valida"<<endl;
+				break;
+		}
+	}
+}
+
+
+void opcion1(){
+	cout<<"Ingresa cadena:"<<endl;
+	string cadena;
+	cin>>cadena;
+	automata(cadena);
+}
+
+void opcion2(){
+	
+	int rnd1,rnd2;
+	string cadena="";
+
+	rnd1=rand()% 1000+2;
+	
+	cout<<"El tamaño de la cadena es: "<<rnd1<<endl;
+	for(int j=0;j<rnd1;j++){
+		rnd2=rand()%2;
+		cadena+=to_string(rnd2);
+	}
+	cout<<cadena<<endl;
+	automata(cadena);
+
+}
+
+void automata(string cad){
+	
+	string q0= "q0",q1= "q1",q2= "q2";
+
+	vector<vector<string>>arr;
+	vector<string>v_aux;
+
+	lim_vec(arr,v_aux,q0);
+
+	for(int i=0;i<cad.size();i++){
+		if(cad[i]=='0'){
+			rec1(arr);
+		}else if(cad[i]=='1'){
+			rec2(arr);
+		}
+	}
+
+	v_aux=arr.back();
+	if(v_aux.back()=="q2"){
+		cout<<"fer <3"<<endl;
+	}else{
+		cout<<"never"<<endl;
+	}
+
+}
+
+void rec1(vector<vector<string>>&arr){
+
+	string q0= "q0",q1= "q1",q2= "q2";
+	vector<string>v_aux;
+	
+	for(int i=0;i<arr.size();i++){
+		v_aux=arr[i];
+		if(v_aux.back()==q0){
+			v_aux.push_back(q0);
+			arr[i]=v_aux;
+		}else if(v_aux.back()==q1){
+			arr.erase(arr.begin()+i); 
+		}else if(v_aux.back()==q2){
+			arr.erase(arr.begin()+i); 	
+		}
+	}
+	lim_vec(arr,v_aux,q1);
+
+}
+
+void rec2(vector<vector<string>>&arr){
+
+	string q0= "q0",q1= "q1",q2= "q2";
+	vector<string>v_aux;
+	
+	for(int i=0;i<arr.size();i++){
+		v_aux=arr[i];
+		if(v_aux.back()==q0){
+			v_aux.push_back(q0);
+			arr[i]=v_aux;
+		}else if(v_aux.back()==q1){
+			v_aux.push_back(q2);
+			arr[i]=v_aux;
+		}else if(v_aux.back()==q2){
+			arr.erase(arr.begin()+i); 	
+		}
+	}
+
+
+}
+
+void lim_vec(vector<vector<string>>&arr,vector<string>&vec, string cad){
+	vec.clear();
+	vec.push_back(cad);
+	arr.push_back(vec);
+	vec.clear();
+}
+
---
+++ Entrega/protocolo.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<fstream>
+#include<ctime>
+#include<windows.h>
+using namespace std;
+
+void GenCadenas(ofstream &file);
+void Clasificador(ofstream &file);
+string Automata(string cadena);
+void Insertdoc(string cadena,ofstream &file);
+
+int main(){
+
+	ofstream file1,file2;
+
+	int rnd,cont=0;
+
+	srand(time(NULL));
+
+	while(true){
+		rnd=rand()%2;
+		if(rnd){
+			cout<<"Estado: Activo "<<++cont<<endl;
+			GenCadenas(file1);
+			Clasificador(file2);
+		}else{
+			cout<<"Apagado."<<endl;
+			break;
+		}
+	}
+	return 0;
+}
+
+void GenCadenas(ofstream &file){
+	
+	int rnd;
+	char c;
+		
+	file.open("cad_aleatorias.txt");
+
+	for(int i=0;i<100;i++){
+		
+		for(int j=0;j<100000;j++){
+			rnd=rand()%2 +48;
+			c=(char)rnd;
+			file<<c;
+		}
+		file<<endl;
+	}
+	file.close();
+}
+
+void Clasificador(ofstream &file){
+	
+	string line ="", ans="";
+	
+	ifstream file1("cad_aleatorias.txt");
+
+	if(file1.is_open()){
+		while(getline(file1,line)){
+			ans= Automata(line);
+			if(ans=="p0"){
+				Insertdoc(line,file);
+			}
+		}
+	}
+
+	file1.close();
+	remove( "cad_aleatorias.txt" );
+	Sleep(2000);
+}
+
+string Automata(string cadena){
+
+		string estados="p0";
+		cout<<estados;
+		for(int i=0;i<cadena.size();i++){
+			if(estados=="p0"){
+				if(cadena[i]=='0') estados="p1";
+				else estados="p3";
+			}else if(estados=="p1"){
+				if(cadena[i]=='0') estados="p0";
+				else estados="p2";
+			}else if(estados=="p2"){
+				if(cadena[i]=='0') estados="p3";
+				else estados="p1";
+			}else if(estados=="p3"){
+				if(cadena[i]=='0') estados="p2";
+				else estados="p0";
+			}
+			cout<<"-"<<estados;
+		}
+		cout<<endl;
+return estados;
+}
+
+void Insertdoc(string cadena,ofstream &file){
+
+	file.open("cadpares.txt",ios_base::app);
+  	file << cadena<<endl; 
+	file.close();
+}
